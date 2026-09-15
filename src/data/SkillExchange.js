635c7431e5215579d09b14f1dd440a4601460909
Shaurya <shauryasethi000@gmail.com>
2026-09-15 15:05:13 +0530

    fixed darkmode issue of skill exchange page

--- src/data/SkillExchange.js
+++ src/data/SkillExchange.js
@@ -13,74 +13,86 @@ export const SkillExchangeData = [
   },
   {
     id: 2,
-    person_name: "Amanpreet Kaur",
+    person_name: "Manan Bansal",
     level_rating: "4.0",
     level: "Easy",
     skills: ["Python", "React", "Java"],
     imageUrl:
-      "https://static.vecteezy.com/system/resources/thumbnails/048/378/879/small_2x/a-young-happy-pretty-smiling-professional-business-woman-free-photo.jpg",
+      "https://tse4.mm.bing.net/th/id/OIP.JIgyNY33dTFe2PR110cP8QHaKd?r=0&w=1417&h=2000&rs=1&pid=ImgDetMain&o=7&rm=3",
     description: "Computer Science, 1st year",
-    email: "amanpreet.kaur@campus.edu",
-    linkedin: "amanpreet-kaur"
+    email: "manan.bansal@campus.edu",
+    linkedin: "manan-bansal"
   },
   {
     id: 3,
-    person_name: "Harman",
-    level_rating: "5.8",
+    person_name: "Vatsal Goyal",
+    level_rating: "4.2",
     level: "Difficult",
     skills: ["Ai", "React", "Java, c++"],
     imageUrl:
       "https://tse3.mm.bing.net/th/id/OIP.hs03TRDnHF7U2YAj7iEsbAHaLH?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
     description: "Computer Science, 4th year",
-    email: "harman@campus.edu",
-    linkedin: "harman"
+    email: "vatsal.goyal@campus.edu",
+    linkedin: "vatsal-goyal"
   },
   {
     id: 4,
-    person_name: "Ananya",
+    person_name: "Sajanpreet Singh Birla",
     level_rating: "4.1",
     level: "Easy",
     skills: ["Python", "React", "Java"],
     imageUrl:
       "https://static.vecteezy.com/system/resources/thumbnails/036/092/706/small_2x/ai-generated-portrait-of-successful-and-happy-businesswoman-in-office-smiling-and-looking-at-camera-with-crossed-arms-photo.jpg",
     description: "Computer Science, 1st year",
-    email: "ananya@campus.edu",
-    linkedin: "ananya"
+    email: "sajanpreet.birla@campus.edu",
+    linkedin: "sajanpreet-birla"
   },
   {
     id: 5,
-    person_name: "Aarna",
+    person_name: "Aftab",
     level_rating: "3.5",
     level: "Easy",
     skills: ["react", "Java", "Python"],
     imageUrl:
-      "https://tse2.mm.bing.net/th/id/OIP.C1QxfhxKco2RW2SGPdcFGQHaLH?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+      "https://static.vecteezy.com/system/resources/thumbnails/049/069/628/small_2x/smiling-muslim-man-with-beard-and-crossed-arms-pose-photo.jpg",
     description: "Computer Science, 1st year",
-    email: "aarna@campus.edu",
-    linkedin: "aarna"
+    email: "aftab@campus.edu",
+    linkedin: "aftab"
   },
   {
     id: 6,
-    person_name: "Rahul",
+    person_name: "Shaurya Sethi",
     level_rating: "4.8",
     level: "Easy",
     skills: ["Full Stack Development", "React", "Node.js"],
     imageUrl:
       "https://img.freepik.com/premium-photo/young-professional-corporate-person_1146775-803.jpg",
     description: "Computer Science, 4th year",
-    email: "rahul@campus.edu",
-    linkedin: "rahul"
+    email: "shaurya@campus.edu",
+    linkedin: "shaurya"
   },
   {
     id: 7,
-    person_name: "Gian",
+    person_name: "Shivansh",
     level_rating: "3.6",
     level: "Easy",
     skills: ["React", "Java"],
     imageUrl:
       "https://cdn.pixabay.com/photo/2015/08/05/10/42/andrew-ronalds-876138_1280.jpg",
     description: "Computer Science, 1st year",
-    email: "gian@campus.edu",
-    linkedin: "gian"
+    email: "shivansh@campus.edu",
+    linkedin: "shivansh"
+  },
+  {
+    id: 8,
+    person_name: "Tejan",
+    level_rating: "5.0",
+    level: "Easy",
+    skills: ["React", "Java", "Python","Full Stack Development", "Node.js"],
+    imageUrl:
+      "https://tse2.mm.bing.net/th/id/OIP.gzp3Wbpv3n9_8Ilr4i0TRwHaHa?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    description: "Computer Science, 4th year",
+    email: "tejan@campus.edu",
+    linkedin: "tejan"
   }
 ];
